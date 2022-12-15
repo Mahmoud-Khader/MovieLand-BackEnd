e5f62c49e90995b668c2736837a457295fd28e1e
MahmoudKhader <82364465+Mahmoud-Khader@users.noreply.github.com>
2022-12-15 14:24:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require('express');
 const cors = require('cors');
 
 const mongoose = require('mongoose');
-mongoose.connect(mongodb+srv://mahmoud:mahmoud@cluster0.gt8fb.mongodb.net/test, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mongodb+srv://mahmoud:mahmoud123456789@cluster0.gt8fb.mongodb.net/?retryWrites=true&w=majority, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 
